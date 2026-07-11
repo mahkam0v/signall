0ee60ca2b6756319bf79b0f32a42faecf8b122a2
OtabekMaxDev <o123mahkamov@gmail.com>
2026-07-11 21:09:55 +0500

    deploying to railway

--- signal-backend/src/db/data-source.js
+++ signal-backend/src/db/data-source.js
@@ -7,19 +7,19 @@ import { Channel } from './entities/Channel.js';
 import { User } from './entities/User.js';
 
 const connectionOptions = env.DATABASE_URL
-  ? { url: env.DATABASE_URL }
+  ? {
+      url: env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
   : {
       host: env.PG.host,
       port: env.PG.port,
       username: env.PG.user,
       password: env.PG.password,
       database: env.PG.database,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     };
-
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  ...connectionOptions,
-  synchronize: false,
-  logging: false,
-  entities: [Anime, Channel, User],
-});
