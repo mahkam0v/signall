2ba8f2072faed8ca16e0b6a2743510a888a59f8c
OtabekMaxDev <o123mahkamov@gmail.com>
2026-07-14 10:15:01 +0500

    fix: env.js

--- signal-backend/src/config/env.js
+++ signal-backend/src/config/env.js
@@ -9,7 +9,7 @@ export const env = {
 
   DATABASE_URL: process.env.DATABASE_URL || null,
   PG: {
-    host: process.env.DATABASE_URL || 'localhost',
+    host: process.env.PGHOST || 'localhost',
     port: Number(process.env.PGPORT || 5432),
     user: process.env.PGUSER || 'postgres',
     password: process.env.PGPASSWORD || 'postgres',
