2093da5130f7453a2588380024541c1729f5d0b8
OtabekMaxDev <o123mahkamov@gmail.com>
2026-07-14 11:39:58 +0500

    fix: DATABESE_URL

--- signal-backend/src/db/data-source.js
+++ signal-backend/src/db/data-source.js
@@ -1,39 +1,3 @@
-// import 'reflect-metadata';
-// import { DataSource } from 'typeorm';
-// import { env } from '../config/env.js';
-
-// import { Anime } from './entities/Anime.js';
-// import { Channel } from './entities/Channel.js';
-// import { User } from './entities/User.js';
-
-// const connectionOptions = env.DATABASE_URL
-//   ? {
-//       url: env.DATABASE_URL,
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-//     }
-//   : {
-//       host: env.PG.host,
-//       port: env.PG.port,
-//       username: env.PG.user,
-//       password: env.PG.password,
-//       database: env.PG.database,
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-//     };
-
-// // 👇 MANA SHU QISMI SIZDA YETISHMAYOTGAN EDI:
-// export const AppDataSource = new DataSource({
-//   type: 'postgres', // TypeORM'ga PostgreSQL ishlatayotganimizni aytamiz
-//   ...connectionOptions, // Yuqoridagi ulanish sozlamalarini ulaymiz
-//   synchronize: true, // Sahifalar/jadvallar avtomatik yaratilishi uchun (Lokalda qulay)
-//   logging: false,
-//   entities: [Anime, Channel, User], // Entity modellarini ro'yxatga olamiz
-//   migrations: [],
-//   subscribers: [],
-// });
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { env } from '../config/env.js';
